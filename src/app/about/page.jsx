eb3c911a0e11326763b6686dd983c1187a623191
Emmanueldesirekouadio <kouadiotiamz@gmail.com>
2024-06-04 15:33:28 +0000

    last

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -11,9 +11,8 @@ function AboutPage() {
           </h2>
           <br />
           <p className={styles.desc}>
-            À Propos de BlaNian.Event Bienvenue sur Eventy, votre destination
-            virtuelle pour l&apos;innovation, l&apos;inspiration et la
-            connexion.
+            Bienvenue sur BlaNian.Event, votre destination virtuelle pour
+            l&apos;innovation, l&apos;inspiration et la connexion.
             <br />
             Chez BlaNian.Event , nous croyons au pouvoir de rassembler les
             esprits créatifs, les entrepreneurs ambitieux et les curieux
@@ -28,7 +27,7 @@ function AboutPage() {
             Que vous soyez un professionnel à la recherche de nouvelles
             collaborations, un créateur désireux de partager vos créations avec
             le monde, ou simplement un enthousiaste de la technologie et de
-            l&apos;innovation, Eventy est l&apos;endroit où vous trouverez
+            l&apos;innovation,BlaNian.Event est l&apos;endroit où vous trouverez
             l&apos;inspiration, l&apos;expertise et les connexions dont vous
             avez besoin pour réaliser vos rêves.
             <br />
